014d2c693913a7b245bcccf4253b3e80a04bd166
chuanxia zheng <chuanxiazheng@engs-30251.local>
2025-06-13 14:49:06 +0100

    fix bugs in the code

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '',
+  basePath: '/physicalvision.github.io',
+  assetPrefix: '/physicalvision.github.io/',
+  trailingSlash: true,  // Highly recommended for GitHub Pages
   images: {
     unoptimized: true
   }
